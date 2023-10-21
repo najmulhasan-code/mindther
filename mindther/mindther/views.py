1d746e6f83a454b7865286b40d6736227b8e2091
Najmul Hasan <123287444+najmulhasan-code@users.noreply.github.com>
2023-10-21 10:59:27 -0400

    Retrieve the response for the Front-End and UI improvement.

--- mindther/mindther/views.py
+++ mindther/mindther/views.py
@@ -1,5 +1,6 @@
 # this is views
 from django.shortcuts import render
+from django.http import JsonResponse
 import openai, os
 from dotenv import load_dotenv
 
@@ -15,12 +16,42 @@ def chatbot(request):
         user_input = request.POST.get('user_input')
         prompt = user_input
 
+<<<<<<< Updated upstream
         response = openai.Completion.create(
             engine='text-davinci-003',
             prompt='prompt',
             max_tokens=256,
             stop="stop",
             temperature=1,
+=======
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+
+                {"role": "system",
+                 "content": "Be Friendly: Use casual, be a friend, relatable language. Say What's up? not How are you "
+                            "today? Stay"
+                            "Upbeat: Be positive. Use emojis and phrases like You got this! Listen Up: Reference past "
+                            "chat and validate feelings. Use phrases like I hear you. No Triggers: Don't bring up "
+                            "self-harm or trauma unless the user does. Redirect if needed. No Med Advice: Stick to "
+                            "emotional support and coping tips. Tone-Match: Mirror the user's emotional state in your "
+                            "tone. Confidential Chat: Remind the user the chat is a safe, confidential space. Crisis "
+                            "Alert: If harmful behavior is mentioned, direct to professional help. Multiple Angles: "
+                            "Offer varied perspectives on problems, with pros and cons. Clear Up: If a query is "
+                            "vague, ask for more info. Tailored Tips: Reference specific details from the user. Wrap "
+                            "It Up: Summarize key points and next steps at the end. Quick Fix: Correct any mistakes "
+                            "in the next message. One More Q: End with an extra question for feedback or more "
+                            "support. Just Friends: Keep it platonic. Redirect any inappropriate topics, "
+                            "do not mention you are ai companion or you are ai try to pretend to be human like as "
+                            "possible. If factual questions are asked try to diverge questions to a funny joke or "
+                            "distractions and do not answer it."},
+
+                {"role": "user", "content": prompt},
+
+            ]
+    
+>>>>>>> Stashed changes
         )
-        print(response)
+        chatbot_response = response.choices[0].message['content']
+        return JsonResponse({'response': chatbot_response})
     return render(request, 'main.html', {})
